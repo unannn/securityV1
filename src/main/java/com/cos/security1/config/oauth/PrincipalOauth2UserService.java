4414a6c58190463f023c1d00678364d684a81335
이윤환 <zoo7930@sju.ac.kr>
2022-05-10 16:38:49 +0900

    페이스북, 구글 로그인 시 미가입 유저는 자동 회원가입

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,8 +1,14 @@
 package com.cos.security1.config.oauth;
 
+import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.FacebookUserInfo;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
+import com.cos.security1.entity.User;
 import com.cos.security1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -10,6 +16,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -17,6 +24,7 @@ import javax.swing.*;
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     //구글로 부터 받은 userRequest 데이터 후처리되는 함수
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -24,13 +32,46 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         log.debug("ClientRegistration = {}", userRequest.getClientRegistration()); //registrationId로 어떤 OAuth로 로그인 했는지 확인가능.
         log.debug("AccessToken = {}", userRequest.getAccessToken().getTokenValue()); //토큰 정보
 
-        // 구글 로그인 버튼 클릭-> 구글 로그인창 -> 로그인 완료 -> code를 리턴(OAuth-Client 라이브러리가 받음) -> AccessToken 요청
-        // userRequest 정보 -> 회원 프로필을 받아야함(loadUser함수를 통해)
-        // 결론 : loadUser를 통해 구글로 부터 회원 프로필을 받아준다.
+
         log.debug("loadUser(userRequest).getAttributes = {}", super.loadUser(userRequest).getAttributes());
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
+        // 구글 로그인 버튼 클릭-> 구글 로그인창 -> 로그인 완료 -> code를 리턴(OAuth-Client 라이브러리가 받음) -> AccessToken 요청
+        // userRequest 정보 -> 회원 프로필을 받아야함(loadUser함수를 통해)
+        // 결론 : loadUser를 통해 구글로 부터 회원 프로필을 받아준다.
 
-        return super.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if (registrationId.equals("google")) {
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        }else if(registrationId.equals("facebook")){
+            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+
+        }else{
+            System.out.println("we support to google or facebook");
+        }
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
+        String username = provider + "_" + providerId;
+        String password = UUID.randomUUID().toString();
+        String email = oAuth2UserInfo.getEmail();
+        String role = "ROLE_USER";
+
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity == null){
+            userEntity = User.builder()
+                    .username(username)
+                    .password(password)
+                    .email(email)
+                    .role(role)
+                    .provider(provider)
+                    .providerId(providerId)
+                    .build();
+            userRepository.save(userEntity);
+        }
+
+        //회원가입을 강제로 진행
+        return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
     }
 }
