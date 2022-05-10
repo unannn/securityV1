43cfde9bc66c1c737c0a2e52f1f69bf80ba92be3
이윤환 <zoo7930@sju.ac.kr>
2022-05-10 16:39:21 +0900

    OAuth 로그인 시 생성자 추가

--- src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -21,12 +21,20 @@ import java.util.Collection;
 import java.util.Map;
 
 @Data
-public class PrincipalDetails implements UserDetails, OAuth2User {
+public class  PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
+    private Map<String, Object> attributes;
 
-    PrincipalDetails(User user) {
+    //일반 로그인지 생성자
+    public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    //OAuth 로그인 시 생성자
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
     }
 
     //해당 User의 권한을 리턴하는 곳
@@ -78,11 +86,11 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Override
     public Map<String, Object> getAttributes() {
-        return null;
+        return attributes;
     }
 
     @Override
     public String getName() {
-        return null;
+        return (String) attributes.get("sub");
     }
 }
